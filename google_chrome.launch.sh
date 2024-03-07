71a851dc5707c8351c82907946a95df6dda3da0a
Robin <robin@tkn-wks-004>
2024-03-07 13:48:39 +0000

    New Chrome launcher.

--- google_chrome.launch.sh
+++ google_chrome.launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PROFILENAME="$1"
+PROFILENAMEDISPLAY="$2"
 
 GCPROFILEPATH=~/apps/google_chrome/profiles/${PROFILENAME}
 GCOPTS="--disable-session-crashed-bubble --enable-leak-detection"
@@ -15,3 +16,36 @@ fi
 CMD="google-chrome ${GCOPTS} --user-data-dir=${GCPROFILEPATH}"
 echo "${CMD}"
 ${CMD} >/dev/null 2>/dev/null &
+
+sleep 1
+
+CHROMEPID=$(pgrep -f "chrome.* --user-data-dir=.*/google_chrome/profiles/${PROFILENAME}$")
+
+TRUEWID=""
+widlist=$(xdotool search --name "Google Chrome")
+for THISWID in ${widlist} ; do
+ THISPID=$(xdotool getwindowpid ${THISWID})
+ if [ "${THISPID}"  == "${CHROMEPID}" ] ; then
+  echo "Matched WID: ${THISWID}"
+  TRUEWID="${THISWID}"
+ fi 
+done 
+
+if [ "${TRUEWID}" == "" ] ; then
+ LOOPACTIVE="no"
+else
+ LOOPACTIVE="yes"
+fi
+
+while [ "${LOOPACTIVE}" == "yes" ] ; do
+ THISWINDOWNAME=$(xdotool getwindowname ${TRUEWID})
+ if [ "${THISWINDOWNAME}" != "${PROFILENAMEDISPLAY}" ] ; then
+  echo "Setting WID: ${THISWID} to ${PROFILENAMEDISPLAY}"
+  xdotool set_window --name "${PROFILENAMEDISPLAY}" ${TRUEWID}
+  if [ $? -ne 0 ] ; then
+   LOOPACTIVE="no"
+  fi
+ fi
+ sleep 1
+done
+
